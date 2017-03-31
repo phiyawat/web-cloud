53d79577a38a17b93413da61839f3d016ed8e582
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2017-04-01 04:01:28 +0700

    5

--- index.js
+++ index.js
@@ -5,29 +5,16 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-var list = require('./list');
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res){
-    res.send('<h1>Hello Node.js</h1>');
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-app.get('/list',function (req, res){
-    res.json(list.findAll());
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
-
-app.get('/list/:id', function(req, res){
-    var id = req.params.id;
-    res.json(list.findById(id));
-});
-
-app.post('/newlist', function(req, res){
-    var json = req.body;
-    res.send('Add new ' + json.name + ' Completed!');
-});
-
-app.get('/index', function(req, res){
-    res.send('<h1>This is index page</h1>');
-});
-
 
 

661b19cb19fedc5087c97bc9021e7043137bf754
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2017-04-01 04:03:53 +0700

    add list

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var list = require('./list');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));

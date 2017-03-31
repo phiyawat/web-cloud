2323b3b4def8ecc5fda9d2f94a4a6d53828d4134
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2017-04-01 04:12:28 +0700

    add listen

--- index.js
+++ index.js
@@ -36,4 +36,8 @@ app.get('/index', function(req, res){
     res.send('<h1>This is index page</h1>');
 });
 
+app.listen(port, function(){
+    console.log('Starting node.js on port' + port);
+});
+
 

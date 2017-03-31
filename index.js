cd469779218998bf33c76c4fff0bae95ede7f6e2
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2017-04-01 03:52:45 +0700

    3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 5000;
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -35,8 +35,5 @@ app.get('/index', function(req, res){
     res.send('<h1>This is index page</h1>');
 });
 
-app.listen(port, function(){
-    console.log('Starting node.js on port' + port);
-});
 
 

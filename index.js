b4559abdd80e651aee1a5bc83cad2fa43a9eabf7
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2017-04-01 04:08:56 +0700

    add all

--- index.js
+++ index.js
@@ -17,7 +17,15 @@ app.get('/list',function (req, res){
     res.json(list.findAll());
 });
 
+app.get('/list/:id', function(req, res){
+    var id = req.params.id;
+    res.json(list.findById(id));
+});
 
+app.post('/newlist', function(req, res){
+    var json = req.body;
+    res.send('Add new ' + json.name + ' Completed!');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));

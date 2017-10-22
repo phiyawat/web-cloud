13291e167ca73442d28bd95ec77e6a19daaf9ed6
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2017-10-22 21:21:38 +0700

    First Commit

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
-var express = require('express');
-var app = express();
-var list = require('./list');
-app.set('port', (process.env.PORT || 5000));
+var express = require('express')
+var app = express()
+var list = require('./list')
+app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
-app.get('/', function (req, res){
-    res.send('<h1>Hello Node.js</h1>');
-});
+app.get('/', function (req, res) {
+  res.send('<h1>Hello Node.js</h1>')
+})
 
-app.get('/list',function (req, res){
-    res.json(list.findAll());
-});
+app.get('/list', function (req, res) {
+  res.json(list.findAll())
+})
 
-app.get('/list/:id', function(req, res){
-    var id = req.params.id;
-    res.json(list.findById(id));
-});
+app.get('/list/:id', function (req, res) {
+  var id = req.params.id
+  res.json(list.findById(id))
+})
 
-app.post('/newlist', function(req, res){
-    var json = req.body;
-    res.send('Add new ' + json.name + ' Completed!');
-});
+app.post('/newlist', function (req, res) {
+  var json = req.body
+  res.send('Add new ' + json.name + ' Completed!')
+})
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
 
-
-app.get('/index', function(req, res){
-    res.send('<h1>This is index page</h1>');
-});
-
-app.listen(port, function(){
-    console.log('Starting node.js on port' + port);
-});
-
-
+app.get('/index', function (req, res) {
+  res.send('<h1>This is index page</h1>')
+})

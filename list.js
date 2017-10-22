911329305fc9e6fc42ea5c0db7d6a704f8ec7fe1
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2017-10-22 18:07:46 +0700

    Update list.js

--- list.js
+++ list.js
@@ -72,15 +72,3 @@ exports.findById = function(id){
         }
     }
 };
-
-exports.findAll = function(){
-    return list;
-}
-
-exports.findById = function(id){
-    for(var i = 0;i < list.length;i++){
-        if(list[i].id == id){
-        return list[i];
-        }
-    }
-};

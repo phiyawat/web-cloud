0c946c6bed0468629ff07afa67dc5afdcc768f60
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2018-06-18 16:33:44 +0700

    allow access-control-allow-origin

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.static(__dirname + '/public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.get('/', function (req, res) {
   res.send('<h1>Hello Node.js</h1>')
 })

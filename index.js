826264ce2985da43ee149a8feb4461db418b6f70
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2018-11-13 19:55:18 +0700

    create server

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var request = require("request");
 app.set("port", process.env.PORT || 5000);
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -39,51 +38,51 @@ app.post("/newlist", function(req, res) {
   res.send("Add new " + json.name + " Completed!");
 });
 
-app.post("/card", function(req, res) {
-  var data = req.body;
-  var dataString =
-    "card[name]=JOHN DOE&card[number]=" +
-    data.number +
-    "&card[security_code]=" +
-    data.code +
-    "&card[expiration_month]=" +
-    data.expireMonth +
-    "&card[expiration_year]=" +
-    data.expireYear;
-  request.post(
-    "https://vault.omise.co/tokens",
-    {
-      auth: {
-        user: publicKey
-      },
-      body: dataString
-    },
-    function(error, response, body) {
-      res.send(body);
-    }
-  );
-});
+// app.post("/card", function(req, res) {
+//   var data = req.body;
+//   var dataString =
+//     "card[name]=JOHN DOE&card[number]=" +
+//     data.number +
+//     "&card[security_code]=" +
+//     data.code +
+//     "&card[expiration_month]=" +
+//     data.expireMonth +
+//     "&card[expiration_year]=" +
+//     data.expireYear;
+//   request.post(
+//     "https://vault.omise.co/tokens",
+//     {
+//       auth: {
+//         user: publicKey
+//       },
+//       body: dataString
+//     },
+//     function(error, response, body) {
+//       res.send(body);
+//     }
+//   );
+// });
 
-app.post("/charges", function(req, res) {
-  var data = req.body;
-  var dataString =
-    "description=Charge for order 3947&amount=" +
-    data.prices +
-    "&currency=thb&return_uri=http://www.example.com/orders/3947/complete&card=" +
-    data.tokens;
-  request.post(
-    "https://api.omise.co/charges",
-    {
-      auth: {
-        user: privateKey
-      },
-      body: dataString
-    },
-    function(error, response, body) {
-      res.send(body);
-    }
-  );
-});
+// app.post("/charges", function(req, res) {
+//   var data = req.body;
+//   var dataString =
+//     "description=Charge for order 3947&amount=" +
+//     data.prices +
+//     "&currency=thb&return_uri=http://www.example.com/orders/3947/complete&card=" +
+//     data.tokens;
+//   request.post(
+//     "https://api.omise.co/charges",
+//     {
+//       auth: {
+//         user: privateKey
+//       },
+//       body: dataString
+//     },
+//     function(error, response, body) {
+//       res.send(body);
+//     }
+//   );
+// });
 
 app.get("/index", function(req, res) {
   res.send("<h1>This is index page</h1>");

95c9f58534a403455ea885d831b1e0b83d7d92a6
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2018-11-13 20:00:10 +0700

    create server

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
 var list = require("./list");
-var bodyParser = require("body-parser");
-var request = require("request");
+// var bodyParser = require("body-parser");
+// var request = require("request");
 app.set("port", process.env.PORT || 5000);
 
 app.use(express.static(__dirname + "/public"));

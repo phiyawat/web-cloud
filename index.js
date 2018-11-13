07addadaed1900ab6b3b8f6578928142e4fdb1d7
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2018-11-13 19:23:49 +0700

    create server

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require("express");
 var app = express();
 var list = require("./list");
@@ -74,47 +73,3 @@ app.post("/charges", function(req, res) {
 
 app.listen(8080);
 console.log("My Service is listening to port 8080.");
-=======
-var express = require('express')
-var app = express()
-var list = require('./list')
-app.set('port', (process.env.PORT || 5000))
-
-app.use(express.static(__dirname + '/public'))
-
-// views is directory for all template files
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs')
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
-
-app.get('/', function (req, res) {
-  res.send('<h1>Hello Node.js</h1>')
-})
-
-app.get('/list', function (req, res) {
-  res.json(list.findAll())
-})
-
-app.get('/list/:id', function (req, res) {
-  var id = req.params.id
-  res.json(list.findById(id))
-})
-
-app.get('/index', function (req, res) {
-  res.send('<h1>This is index page</h1>')
-})
-
-app.post('/newlist', function (req, res) {
-  var json = req.body
-  res.send('Add new ' + json.name + ' Completed!')
-})
-
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'))
-})
->>>>>>> 0c946c6bed0468629ff07afa67dc5afdcc768f60
--- list.js
+++ list.js
@@ -1,5 +1,4 @@
 var list =
-<<<<<<< HEAD
 {
   id: [
     {
@@ -15,109 +14,6 @@ var list =
 }
 
 exports.findAll = function () {
-=======
-[
-  {
-    type: 'ONE_WAY',
-    outbound: {
-      airline: 'AeroFlot',
-      direction: 'OUTBOUND',
-      price: 636.0,
-      takeOffFrom: 'Suvanabhumi, Terminal',
-      takeOffTime: {
-        year: 2017,
-        month: 10,
-        dayOfMonth: 28,
-        hourOfDay: 10,
-        minute: 40,
-        second: 0
-      },
-      landingTo: 'Paris-Charles-de-Gaulle, Terminal2C',
-      landingTime: {
-        year: 2017,
-        month: 10,
-        dayOfMonth: 29,
-        hourOfDay: 22,
-        minute: 30,
-        second: 0
-      },
-      transists: [
-        {
-          fromAirport: 'Suvanabhumi, Terminal',
-          toAirport: 'Sheremetyevo, Terminal F',
-          order: 1,
-          flightNO: 'SU271',
-          aircraftType: 'Airbus333',
-          airline: 'Aeroflot',
-          seatClass: 'ECONOMY',
-          remainingSeats: 4
-        },
-        {
-          fromAirport: 'Sheremetyevo Terminal',
-          toAirport: 'Paris-Charles-De-Gaulle, Terminal2C',
-          order: 2,
-          flightNO: 'SU2460',
-          aircraftType: 'Airbus320',
-          airline: 'Aeroflot',
-          seatClass: 'ECONOMY',
-          remainingSeats: 4
-        }
-      ]
-    }
-  },
-  {
-    type: 'ONE_WAY',
-    outbound: {
-      airline: 'Thai Airways',
-      direction: 'OUTBOUND',
-      price: 930.0,
-      takeOffFrom: 'Suvanabhumi, Terminal',
-      takeOffTime: {
-        year: 2017,
-        month: 10,
-        dayOfMonth: 28,
-        hourOfDay: 23,
-        minute: 40,
-        second: 0
-      },
-      landingTo: 'Paris-Charles-de-Gaulle, Terminal1A',
-      landingTime: {
-        year: 2017,
-        month: 10,
-        dayOfMonth: 29,
-        hourOfDay: 8,
-        minute: 30,
-        second: 0
-      },
-      transists: [
-        {
-          fromAirport: 'Suvanabhumi, Terminal',
-          toAirport: 'Brisbane Airport, Terminal D',
-          order: 1,
-          flightNO: 'TG930',
-          aircraftType: 'Airbus320',
-          airline: 'Thai Airways',
-          seatClass: 'FIRST_CLASS',
-          remainingSeats: 10
-        },
-        {
-          fromAirport: 'Brisbane Airport, Terminal D',
-          toAirport: 'Paris-Charles-De-Gaulle, Terminal1A',
-          order: 2,
-          flightNO: 'TG933',
-          aircraftType: 'Airbus320',
-          airline: 'Thai Airways',
-          seatClass: 'BUSINESS',
-          remainingSeats: 10
-        }
-      ]
-    }
-  }
-]
-
-exports.findAll = function () {
-  console.log('List: ' + list)
->>>>>>> 0c946c6bed0468629ff07afa67dc5afdcc768f60
   return list
 }
 

600742b577b4bd025359d43dd090ce1a8df83c61
Phiyawat Vongsawat <phiyawat9597@gmail.com>
2019-01-09 23:35:40 +0700

    add cancelUrl

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.post("/linePay", function(req, res) {
     currency: "THB",
     orderId: "20140101123456789",
     confirmUrl: "https://my-project-9d06f.firebaseapp.com/BuyComplete",
-    cancelUrl: "https://my-project-9d06f.firebaseapp.com/Cancel"
+    cancelUrl: "https://my-project-9d06f.firebaseapp.com/BuyCancel"
   };
   request.post(
     "https://sandbox-api-pay.line.me/v2/payments/request",
